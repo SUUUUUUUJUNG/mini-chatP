36ea2961e102a580a37b6d21959d746aa2d688ef
SHIN SUJUNG <shinssjj79@gmail.com>
2024-11-08 21:14:29 +0900

    Initial commit

---
+++ src/main/java/com/example/mini_chatP/MiniChatPApplication.java
@@ -0,0 +1,13 @@
+package com.example.mini_chatP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniChatPApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniChatPApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mini_chatP/MiniChatPApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mini_chatP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniChatPApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
